6fe733966349bfd314e04f943a0427ab32dab2aa
Remi Lehe <remi.lehe@normalesup.org>
2025-05-15 05:57:41 -0700

    Update simulation script

--- docs/example_simulations/single_stage/wake_t_cylindrical.py
+++ docs/example_simulations/single_stage/wake_t_cylindrical.py
@@ -5,21 +5,7 @@ from wake_t import ParticleBunch, GaussianPulse, PlasmaStage, Beamline
 # Fix random seed for reproducibility
 np.random.seed(0)
 
-def trapezoidal_bunch(
-    i0,
-    i1,
-    n_part,
-    gamma0,
-    s_g,
-    length,
-    s_z,
-    emit_x,
-    s_x,
-    emit_y,
-    s_y,
-    x_c=0.0,
-    y_c=0.0,
-):
+def trapezoidal_bunch( i0, i1, n_part, gamma0, s_g, length, s_z, emit_x, s_x, emit_y, s_y):
     """Create a trapezoidal particle bunch."""
     n_part = int(n_part)
 
@@ -35,27 +21,20 @@ def trapezoidal_bunch(
     n_gaus1 = int(n_part * q_gaus1 / q_tot)
 
     z_plat = np.random.uniform(0.0, length, n_plat)
-    if i0 <= i1:
-        z_triag = np.random.triangular(0.0, length, length, n_triag)
-    else:
-        z_triag = np.random.triangular(0.0, 0.0, length, n_triag)
+    z_triag = np.random.triangular(0.0, length, length, n_triag)
     z_gaus0 = s_z * np.random.standard_normal(2 * n_gaus0)
     z_gaus1 = s_z * np.random.standard_normal(2 * n_gaus1)
 
-    z = np.concatenate(
-        (
-            z_gaus0[np.where(z_gaus0 < 0)],
-            z_plat,
-            z_triag,
-            z_gaus1[np.where(z_gaus1 > 0)] + length,
-        )
-    )
-
+    z = np.concatenate((
+        z_gaus0[np.where(z_gaus0 < 0)],
+        z_plat, z_triag,
+        z_gaus1[np.where(z_gaus1 > 0)] + length,
+    ))
     z = z - length / 2.0  # shift to final position
 
     n_part = len(z)
-    x = x_c + s_x * np.random.standard_normal(n_part)
-    y = y_c + s_y * np.random.standard_normal(n_part)
+    x = s_x * np.random.standard_normal(n_part)
+    y = s_y * np.random.standard_normal(n_part)
 
     gamma = np.random.normal(gamma0, s_g, n_part)
     s_ux = emit_x / s_x
@@ -87,7 +66,7 @@ def matched_beam_size(m_species, E_kin_GeV, beta_matched, norm_emittance):
 
 # General simulation parameters.
 n_p0 = 1.7e23
-q_tot = 196 * 1e-12  # total charge (C) .. is smaller than the actual charge will be due to the Gaussian tails (should come out at 200 pC)
+q_tot = 196 * 1e-12  # total charge (C)
 lambda_L = 0.8e-6  # laser wavelength (m)
 
 # Energy values and corresponding beta function values from calculations and measurements
@@ -102,154 +81,129 @@ beam_i_r2 = 0.469660
 beam_z_i_2 = 2.973740
 beam_length = 5.453219
 
-def simulate_plasma_stage(beam_i_r2=0.8, beam_z_i_2=0.0, beam_length_um=5, beam_size_rms=None,
-                          sz_beam=1e-6, num_ppc=2, num_snapshots=500, diags=True, show_progress_bar=False):
-    # Laser parameters
-    # ----------------
-    a_0 = 2.36
-    w_0 = 36e-6
-    tau_fwhm = 7.33841e-14 * np.sqrt(2.0 * np.log(2))  # FWHM in intensity
-    z_foc = 0.0  # Focal position of the laser
+# Laser parameters
+# ----------------
+a_0 = 2.36
+w_0 = 36e-6
+tau_fwhm = 7.33841e-14 * np.sqrt(2.0 * np.log(2))  # FWHM in intensity
+z_foc = 0.0  # Focal position of the laser
 
-    # Beam parameters
-    # ---------------
-    i = 0  # chooses the beam energy (1 GeV for i = 0) and beta values from above
-    z_beam = (60 + beam_z_i_2) * 1e-6  # distance of the beam center to the drive laser pulse
-    l_beam = beam_length_um * 1e-6  # beam length (m)
+# Beam parameters
+# ---------------
+i = 0  # chooses the beam energy (1 GeV for i = 0) and beta values from above
+z_beam = (60 + beam_z_i_2) * 1e-6  # distance of the beam center to the drive laser pulse
+l_beam = beam_length * 1e-6  # beam length (m)
 
-    # beam currents are coupled due to total charge
-    beam_i_r1 = 1.0 - beam_i_r2
+# beam currents are coupled due to total charge
+beam_i_r1 = 1.0 - beam_i_r2
 
-    # we keep the beam charge almost constant (just not accounting for the gaussian flanks yet)
-    i_avg = q_tot / l_beam * sc.c  # average beam current (for rectangle profile)
-    i1_beam = 2 * beam_i_r1 * i_avg
-    i2_beam = 2 * beam_i_r2 * i_avg
+# we keep the beam charge almost constant (just not accounting for the gaussian flanks yet)
+i_avg = q_tot / l_beam * sc.c  # average beam current (for rectangle profile)
+i1_beam = 2 * beam_i_r1 * i_avg
+i2_beam = 2 * beam_i_r2 * i_avg
 
-    n_part = 50000
-    gamma_beam = gamma_from_GeV(m_species=sc.electron_mass, E_kin_GeV=energies_GeV[i])  # calculate beam gamma
-    ene_sp = 0.1  # energy spread in %
-    sz0 = sz_beam  # gaussian decay
-    n_emitt_x = 1e-6
-    n_emitt_y = 1e-6
-    betax0 = betas_opt[i]
+n_part = 50000
+gamma_beam = gamma_from_GeV(m_species=sc.electron_mass, E_kin_GeV=energies_GeV[i])  # calculate beam gamma
+ene_sp = 0.1  # energy spread in %
+sz_beam = 1e-7
+n_emitt_x = 1e-6
+n_emitt_y = 1e-6
+betax0 = betas_opt[i]
 
-    if beam_size_rms is None:
-        sx0 = np.sqrt(n_emitt_x * betax0 / gamma_beam)  # matched beam size (rms)
-        sy0 = np.sqrt(n_emitt_y * betax0 / gamma_beam)  # matched beam size (rms)
-    else:
-        sx0 = beam_size_rms
-        sy0 = beam_size_rms
+sx0 = np.sqrt(n_emitt_x * betax0 / gamma_beam)  # matched beam size (rms)
+sy0 = np.sqrt(n_emitt_y * betax0 / gamma_beam)  # matched beam size (rms)
 
-    # Generate bunch
-    x, y, z, ux, uy, uz, q = trapezoidal_bunch(
-        i1_beam,
-        i2_beam,
-        n_part=n_part,
-        gamma0=gamma_beam,
-        s_g=ene_sp * gamma_beam / 100,
-        length=l_beam,
-        s_z=sz0,
-        emit_x=n_emitt_x,
-        s_x=sx0,
-        emit_y=n_emitt_y,
-        s_y=sy0,
-    )
-
-    z -= l_beam / 2 + z_beam  # shift the longitudinal beam position away from the driver
-    w = np.abs(q / sc.e)
-    bunch = ParticleBunch(w, x, y, z, ux, uy, uz, name="bunch")
-
-    # Plasma density profile
-    # ----------------------
-    l_plateau = 28.0e-2 # (m)  # SH change for short test
-
-    # Determine guiding channel.
-    r_e = sc.e**2 / (4.0 * np.pi * sc.epsilon_0 * sc.m_e * sc.c**2)
-
-    # empirical length scale for guiding the pulse in this partial blowout
-    emp_len_um = 40.e-6
-    rel_delta_n_over_w2 = 1.0 / (np.pi * r_e * emp_len_um ** 4 * n_p0)
-
-    # Density function.
-    def density_profile(z):
-        """ Define plasma density as a function of ``z``. """
-        # Allocate relative density array.
-        n = np.zeros_like(z)
-        # Add plateau.
-        n = np.where(
-            (z >= 0) & (z <= l_plateau),
-            1, n
-        )
-        # Return absolute density.
-        n_abs = n * n_p0
-        # Add minimal density value everywhere where it is zero, so we avoid crashes
-        # Date: 2024-01-10, recommended by Angel Ferran-Pousa
-        n_fixed = np.where(n_abs <= 1e10, 1e10, n_abs)
-        return n_fixed
-
-    # Create plasma stage
-    # -------------------
-    # Simulation box and grid parameters.
-    r_max = w_0 * 4  # Maximum radial extent of the box
-    r_max_plasma = w_0 * 3  # Maximum radial extent of the plasma.
-    l_box = 200e-6  # Length of simulation box
-    # make sure initialize the bunch with a distance to the driver, or change this number
-    xi_0 = 0  # Center of the drive laser pulse
-    xi_max = xi_0 + 45e-6  # Right edge of the box in the speed-of-light frame
-    xi_min = xi_max - l_box  # Left edge of the box in the speed-of-light frame
-    res_beam_r = 5.  # default: 5, resolution we want for the beam radius
-    dr = np.min([sx0,sy0]) / res_beam_r  # make the radial resolution depend on the RMS beam size to avoid artifacts
-
-    n_e_n_c = n_p0 / calculate_critical_density(wavelength=lambda_L)
-    kp = 2 * np.pi * np.sqrt(n_e_n_c) / lambda_L
-    kpdz_inv = 40 # 1 / (kp * dz), resolution parameter where typically 20 - 40 is good
-    #dz = tau_fwhm / 40 * sc.c  # 1 / (kp * dz) is about 20 in case 7.33841e-14 * np.sqrt(2.0 * np.log(2))
-    dz = 1 / kpdz_inv / kp
-    nr = int(r_max / dr)
-    nz = int(l_box / dz)
-    laser_evolution = True
-
-    laser = GaussianPulse(xi_c=xi_0, a_0=a_0, w_0=w_0, l_0=lambda_L, tau=tau_fwhm, z_foc=z_foc)
-
-    num_snapshots_plateau = num_snapshots  # all but the last will be deleted if analysis_script.py is run
-
-    # Create plasma stages.
-    plasma_plateau = PlasmaStage(
-        length=l_plateau,
-        density=density_profile,
-        wakefield_model="quasistatic_2d",  # this points to the Baxevanis model with ion motion now
-        n_out=num_snapshots_plateau,
-        laser=laser,
-        laser_evolution=laser_evolution,
-        r_max=r_max,
-        r_max_plasma=r_max_plasma,
-        xi_min=xi_min,
-        xi_max=xi_max,
-        n_r=nr,
-        n_xi=nz,
-        dz_fields= 1 * l_box,
-        ppc=num_ppc,
-        parabolic_coefficient=rel_delta_n_over_w2,
-        laser_envelope_substeps=4,
-        laser_envelope_nxi=nz * 4,
-        max_gamma=25
-    )
-
-    # Do tracking
-    # -----------
-    beamline = Beamline([
-        plasma_plateau,
-    ])
-
-    beamline.track(bunch, opmd_diag=diags, show_progress_bar=show_progress_bar, diag_dir='diags')
-
-simulate_plasma_stage(
-    beam_i_r2=beam_i_r2,
-    beam_z_i_2=beam_z_i_2,
-    beam_length_um=beam_length,
-    sz_beam=1e-7,
-    num_ppc=2,
-    num_snapshots=50,
-    diags=True,
-    show_progress_bar=True
+# Generate bunch
+x, y, z, ux, uy, uz, q = trapezoidal_bunch(
+    i1_beam,
+    i2_beam,
+    n_part=n_part,
+    gamma0=gamma_beam,
+    s_g=ene_sp * gamma_beam / 100,
+    length=l_beam,
+    s_z=sz_beam,
+    emit_x=n_emitt_x,
+    s_x=sx0,
+    emit_y=n_emitt_y,
+    s_y=sy0,
 )
+
+z -= l_beam / 2 + z_beam  # shift the longitudinal beam position away from the driver
+w = np.abs(q / sc.e)
+bunch = ParticleBunch(w, x, y, z, ux, uy, uz, name="bunch")
+
+# Plasma density profile
+# ----------------------
+l_plateau = 28.0e-2 # (m)
+# Determine guiding channel.
+r_e = sc.e**2 / (4.0 * np.pi * sc.epsilon_0 * sc.m_e * sc.c**2)
+
+# empirical length scale for guiding the pulse in this partial blowout
+emp_len_um = 40.e-6
+rel_delta_n_over_w2 = 1.0 / (np.pi * r_e * emp_len_um ** 4 * n_p0)
+
+# Density function.
+def density_profile(z):
+    """ Define plasma density as a function of ``z``. """
+    # Allocate relative density array.
+    n = np.zeros_like(z)
+    # Add plateau.
+    n = np.where(
+        (z >= 0) & (z <= l_plateau),
+        1, n
+    )
+    # Return absolute density.
+    n_abs = n * n_p0
+    # Add minimal density value everywhere where it is zero, so we avoid crashes
+    # Date: 2024-01-10, recommended by Angel Ferran-Pousa
+    n_fixed = np.where(n_abs <= 1e10, 1e10, n_abs)
+    return n_fixed
+
+# Create plasma stage
+# -------------------
+# Simulation box and grid parameters.
+r_max = w_0 * 4  # Maximum radial extent of the box
+r_max_plasma = w_0 * 3  # Maximum radial extent of the plasma.
+l_box = 200e-6  # Length of simulation box
+# make sure initialize the bunch with a distance to the driver, or change this number
+xi_0 = 0  # Center of the drive laser pulse
+xi_max = xi_0 + 45e-6  # Right edge of the box in the speed-of-light frame
+xi_min = xi_max - l_box  # Left edge of the box in the speed-of-light frame
+res_beam_r = 5.  # default: 5, resolution we want for the beam radius
+dr = np.min([sx0,sy0]) / res_beam_r  # make the radial resolution depend on the RMS beam size to avoid artifacts
+
+n_e_n_c = n_p0 / calculate_critical_density(wavelength=lambda_L)
+kp = 2 * np.pi * np.sqrt(n_e_n_c) / lambda_L
+kpdz_inv = 40 # 1 / (kp * dz), resolution parameter where typically 20 - 40 is good
+dz = 1 / kpdz_inv / kp
+nr = int(r_max / dr)
+nz = int(l_box / dz)
+laser_evolution = True
+
+laser = GaussianPulse(xi_c=xi_0, a_0=a_0, w_0=w_0, l_0=lambda_L, tau=tau_fwhm, z_foc=z_foc)
+
+# Create plasma stages.
+plasma_plateau = PlasmaStage(
+    length=l_plateau,
+    density=density_profile,
+    wakefield_model="quasistatic_2d",
+    n_out=50,
+    laser=laser,
+    laser_evolution=laser_evolution,
+    r_max=r_max,
+    r_max_plasma=r_max_plasma,
+    xi_min=xi_min,
+    xi_max=xi_max,
+    n_r=nr,
+    n_xi=nz,
+    dz_fields= 1 * l_box,
+    ppc=2,
+    parabolic_coefficient=rel_delta_n_over_w2,
+    laser_envelope_substeps=4,
+    laser_envelope_nxi=nz * 4,
+    max_gamma=25
+)
+
+# Track the beam through the plasma stage
+beamline = Beamline([ plasma_plateau ])
+beamline.track(bunch, opmd_diag=True, show_progress_bar=True, diag_dir='diags')
