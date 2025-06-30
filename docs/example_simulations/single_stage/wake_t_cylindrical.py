7077b29115d417cf12f045caaa0b3f5de03e459d
delaossa <alberto.martinez.de.la.ossa@desy.de>
2025-06-30 12:06:15 +0200

    Restructure the script a bit.

--- docs/example_simulations/single_stage/wake_t_cylindrical.py
+++ docs/example_simulations/single_stage/wake_t_cylindrical.py
@@ -2,12 +2,14 @@ import numpy as np
 import scipy.constants as sc
 from wake_t import ParticleBunch, GaussianPulse, PlasmaStage, Beamline
 
-# Laser parameters
-# ----------------
+# Laser setup
+# -----------
 a_0 = 2.36
 w_0 = 36e-6
 tau_fwhm = 7.33841e-14 * np.sqrt(2.0 * np.log(2))  # FWHM in intensity
+xi_0 = 0  # Center of the drive laser pulse
 lambda_L = 0.8e-6  # laser wavelength (m)
+laser = GaussianPulse(xi_c=xi_0, a_0=a_0, w_0=w_0, l_0=lambda_L, tau=tau_fwhm)
 
 # Plasma density profile
 # ----------------------
@@ -19,30 +21,50 @@ matched_channel_waist = 40.e-6 # determined empirically for best laser guiding
 # matched_channel_waist should be equal to w_0 for low a0, but differs for high a0 due to relativistic guiding
 rel_delta_n_over_w2 = 1.0 / (np.pi * r_e * matched_channel_waist ** 4 * n_p0)
 
-# Beam parameters
-# ---------------
+# Density function.
+def density_profile(z, r):
+    """ Define plasma density as a function of ``z`` and ``r``. """
+    # Allocate relative density array.
+    n = np.zeros_like(z)
+    # Add plateau.
+    n = np.where(
+        (z >= 0) & (z <= l_plateau),
+        1, n
+    )
+    # Return absolute density.
+    n = n * n_p0
+    # Add minimal density value everywhere where it is zero, so we avoid crashes
+    # Date: 2024-01-10, recommended by Angel Ferran-Pousa
+    n = np.where(n <= 1e10, 1e10, n)
+    # Add radial parabolic profile
+    n = n * (1. + rel_delta_n_over_w2 * r**2)
+    return n
+
+# Witness beam setup
+# ------------------
 q_tot = 196 * 1e-12  # total charge (C)
-l_beam = 5.453219e-6  # beam length (m)
-sz_beam = 1e-7
 ene_sp = 0.1  # energy spread in %
 n_emitt_x = 1e-6
 n_emitt_y = 1e-6
+
 # Energy values and corresponding beta function values from calculations and measurements
 #   (previous optimization at 1 fC charge)
 energies_GeV = np.array([1,10,100,1e3,1e4])  # initial beam energy
 betas_opt = np.array([9.330e-4, 2.775e-3, 7.746e-6, 1.721e-2, 3.644e-2])  # measured in optimization study
 i = 0  # chooses the beam energy (1 GeV for i = 0) and beta values from above
+
 # Best values from the case of 086_APOSSMM..
-beam_i_r2 = 0.469660
+l_beam = 5.453219e-6  # beam length (m)
 beam_z_i_2 = 2.973740
 z_beam = (60 + beam_z_i_2) * 1e-6  # distance of the beam center to the drive laser pulse
 # beam currents are coupled due to total charge
+beam_i_r2 = 0.469660
 beam_i_r1 = 1.0 - beam_i_r2
+sz_beam = 1e-7  # gaussian flank size
 # we keep the beam charge almost constant (just not accounting for the gaussian flanks yet)
 i_avg = q_tot / l_beam * sc.c  # average beam current (for rectangle profile)
 i1_beam = 2 * beam_i_r1 * i_avg
 i2_beam = 2 * beam_i_r2 * i_avg
-
 
 # Fix random seed for reproducibility
 np.random.seed(0)
@@ -92,11 +114,6 @@ def gamma_from_GeV(m_species,E_kin_GeV):
     E_rest_GeV = m_species * sc.c**2 / sc.electron_volt / 1e9
     return 1 + E_kin_GeV / E_rest_GeV
 
-def calculate_critical_density(wavelength):
-    """Calculate the critical density for a given laser wavelength."""
-    omega_L = 2 * np.pi * sc.c / wavelength
-    return sc.m_e * sc.epsilon_0 * omega_L**2 / sc.elementary_charge**2
-
 n_part = 50000
 gamma_beam = gamma_from_GeV(m_species=sc.electron_mass, E_kin_GeV=energies_GeV[i])  # calculate beam gamma
 betax0 = betas_opt[i]
@@ -121,23 +138,6 @@ z -= l_beam / 2 + z_beam  # shift the longitudinal beam position away from the d
 w = np.abs(q / sc.e)
 bunch = ParticleBunch(w, x, y, z, ux, uy, uz, name="bunch")
 
-# Density function.
-def density_profile(z):
-    """ Define plasma density as a function of ``z``. """
-    # Allocate relative density array.
-    n = np.zeros_like(z)
-    # Add plateau.
-    n = np.where(
-        (z >= 0) & (z <= l_plateau),
-        1, n
-    )
-    # Return absolute density.
-    n_abs = n * n_p0
-    # Add minimal density value everywhere where it is zero, so we avoid crashes
-    # Date: 2024-01-10, recommended by Angel Ferran-Pousa
-    n_fixed = np.where(n_abs <= 1e10, 1e10, n_abs)
-    return n_fixed
-
 # Create plasma stage
 # -------------------
 # Simulation box and grid parameters.
@@ -145,11 +145,15 @@ r_max = w_0 * 4  # Maximum radial extent of the box
 r_max_plasma = w_0 * 3  # Maximum radial extent of the plasma.
 l_box = 200e-6  # Length of simulation box
 # make sure initialize the bunch with a distance to the driver, or change this number
-xi_0 = 0  # Center of the drive laser pulse
 xi_max = xi_0 + 45e-6  # Right edge of the box in the speed-of-light frame
 xi_min = xi_max - l_box  # Left edge of the box in the speed-of-light frame
 res_beam_r = 5.  # default: 5, resolution we want for the beam radius
 dr = np.min([sx0,sy0]) / res_beam_r  # make the radial resolution depend on the RMS beam size to avoid artifacts
+
+def calculate_critical_density(wavelength):
+    """Calculate the critical density for a given laser wavelength."""
+    omega_L = 2 * np.pi * sc.c / wavelength
+    return sc.m_e * sc.epsilon_0 * omega_L**2 / sc.elementary_charge**2
 
 n_e_n_c = n_p0 / calculate_critical_density(wavelength=lambda_L)
 kp = 2 * np.pi * np.sqrt(n_e_n_c) / lambda_L
@@ -157,9 +161,6 @@ kpdz_inv = 40 # 1 / (kp * dz), resolution parameter where typically 20 - 40 is g
 dz = 1 / kpdz_inv / kp
 nr = int(r_max / dr)
 nz = int(l_box / dz)
-laser_evolution = True
-
-laser = GaussianPulse(xi_c=xi_0, a_0=a_0, w_0=w_0, l_0=lambda_L, tau=tau_fwhm)
 
 # Create plasma stages.
 plasma_plateau = PlasmaStage(
@@ -168,7 +169,7 @@ plasma_plateau = PlasmaStage(
     wakefield_model="quasistatic_2d",
     n_out=50,
     laser=laser,
-    laser_evolution=laser_evolution,
+    laser_evolution=True,
     r_max=r_max,
     r_max_plasma=r_max_plasma,
     xi_min=xi_min,
@@ -177,12 +178,15 @@ plasma_plateau = PlasmaStage(
     n_xi=nz,
     dz_fields= 1 * l_box,
     ppc=2,
-    parabolic_coefficient=rel_delta_n_over_w2,
+    # parabolic_coefficient=rel_delta_n_over_w2,  # deprecated: use density_profile instead
     laser_envelope_substeps=4,
     laser_envelope_nxi=nz * 4,
-    max_gamma=25
+    max_gamma=25,
+    field_diags=["rho", "E", "a"],
 )
 
-# Track the beam through the plasma stage
-beamline = Beamline([ plasma_plateau ])
-beamline.track(bunch, opmd_diag=True, show_progress_bar=True, diag_dir='diags')
+if __name__ == "__main__":
+
+    # Track the beam through the plasma stage
+    beamline = Beamline([ plasma_plateau ])
+    beamline.track(bunch, opmd_diag=True, show_progress_bar=True, diag_dir='diags')
